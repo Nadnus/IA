02172edb50dd9f183c4961a05dbd7eba1364e7fd
Nadnus <fernando.delosheros@utec.edu.pe>
2020-10-15 14:07:39 -0500

    marco

---
+++ lab.py
@@ -0,0 +1,34 @@
+import pandas as pd
+def conteo_interseccion(df,parametros,columnas):
+    return 0
+def estimar_marginal(df,X,a):
+    d = {}
+    
+    for i in df[X]:
+        if i in d :
+            d[i]+=1
+        else:
+            d[i]=1
+    card = len(d)
+    for i in d:
+        d[i] = (d[i]+a)/(df.shape[0] + card*a)
+    
+    dfRes = pd.DataFrame(d,columns = ["X", "P(X)"])
+    return dfRes
+
+def probabilidad_x_si_y(df,x,columnaY,valY,a):
+    toReturn = {}
+    columnas = [x,columnaY]
+    #faltan separar las variables en variables []
+    for row in variables[x]:
+        parametros = [row,valY]
+        card = conteo_interseccion(df,parametros,columnas) + a
+        denom = df.size +  cardinalidad(row)*cardinalidad(valY)*a
+        numerador = card / denom
+        #confirmar los valores del segundo denominador, en la pizarra sale c * y * a,  sera cy *a?
+        denom2 = (conteo(y) +a)/(df.size + cardinalidadVariablesTotal * )
+        totes= numerador / denom2
+        toReturn[row] = (totes)
+    return toReturn
+
+
