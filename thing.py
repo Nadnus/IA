e1555465088ea1eca2d3c3067fc4f38e7619000e
Nadnus <fernando.delosheros@utec.edu.pe>
2020-11-01 00:20:38 -0500

    Acabe lo del acumulador

--- thing.py
+++ thing.py
@@ -87,6 +87,33 @@ def condicionalFijas(df,fijas_nombre,fijas,variables_usadas = []):
     valid_combos  = list(itertools.product(*valid_values))
     return valid_combos
 
-    
+#variables es una lista de listas, condicionales una lista de diccionarios; variables[0] deberia ser la lista de variables que se usan en condicionales[0]
+#
+
+#
+def probVar(df,var,nombres_fijas,valores_fijas,factores,a):
+    valores_var = lb.find_variables(df,var)
+    to_return = {}
+    for valor in valores_var:
+        mult = 1
+        for f in factores:
+            target = (f[0])[0]
+            primers = (f[0])[1]
+            param1 = [var] + nombres_fijas
+            param2 = [valor] + valores_fijas
+            param3 = [target] + primers
+            combos = condicionalFijas(df,param1,param2, param3)
+            for combo in combos:
+                my_cond = f[1]
+                mult = mult * my_cond[combo]
+        to_return[valor] = mult
+    return to_return
+        
+
+
+
 data = pd.read_csv('weather.csv')
-print(condicionalFijas(data,['outlook','windy'],['overcast','TRUE']))
+#print(condicionalFijas(data,['play','windy'],['yes','TRUE']))
+c = condicional(data,'outlook',['windy','play'],1,True)
+c2 = condicional(data,'play',['outlook','windy'],1,True)
+print(probVar(data,'outlook',['play','windy'],['yes',True],[c,c2],1))
